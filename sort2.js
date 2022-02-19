4a79faffb3d3734fe1bc8813e08f2af136c4f6bd
Alla Anderson <76411309+allamalitska@users.noreply.github.com>
2022-02-19 14:12:52 +0200

    Task2

--- sort2.js
+++ sort2.js
@@ -1,20 +1,23 @@
-// const createUser = function(name,age){
-//     const createUsers = new Object({name,age})
-//         Object.prototype.toString = function (){
-//             return `${this.name} is ${this.age} y.o.`;
-//         }
-//     return createUsers
-// }
-
-// const users = [
-//     createUser("Joinh", 21),
-//     createUser("Tory", 18),                       
-//     createUser("Kate", 23),
-//     createUser("Pete", 22)
-// ]
+const createUser = function(name,age){
+    const createUsers = new Object({name,age})
+    Object.prototype.toString = function (){
+        return `${this.name} is ${this.age} y.o.`;}
+    return createUsers
+}
 
 
+let users = [
+    createUser("Joinh", 21),
+    createUser("Tory", 18),                       
+    createUser("Kate", 23),
+    createUser("Pete", 22)
+]
+    
+sortedUsers = [];
+for (let i = 0; i < users.length; i++) {
+    sortedUsers[users[i].age - 1] = users[i];
+}   
+console.log(sortedUsers.toString())
+    
 
-// console.log(Object.values(users))
 
-// console.log(users.sort((a,b) => a.age - b.age).toString())
